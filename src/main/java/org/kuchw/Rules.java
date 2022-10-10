05afc1023ae98b08daae3d111dea5f28ce93680f
wkuch <wanjakuch@gmail.com>
2022-10-10 21:03:12 +0200

    some rules refactoring

--- src/main/java/org/kuchw/Rules.java
+++ src/main/java/org/kuchw/Rules.java
@@ -25,6 +25,7 @@ public class Rules {
 
     public static Hand findBetterHand(Hand firstHand, Hand secondHand) {
 
+        //Handle case when hand types are different
         for(int i = 0; i < firstHand.getHandType().length; i++) {
             int evaluation = firstHand.getHandType()[i] - secondHand.getHandType()[i];
 
@@ -36,70 +37,36 @@ public class Rules {
 
         }
 
+        //Handle cases when hand types are the same
         if(firstHand.getHandType()[0] == 1 || firstHand.getHandType()[3] == 1 || firstHand.getHandType()[4] == 1){
 
-            if(getCardValueIndex(firstHand.getCards()[0].getValue()) > getCardValueIndex(secondHand.getCards()[0].getValue())){
-                return firstHand;
-            } else if(getCardValueIndex(firstHand.getCards()[0].getValue()) < getCardValueIndex(secondHand.getCards()[0].getValue())){
-                return secondHand;
-            } else {
-                return null;
-            }
+            return handleFlushAndStraight(firstHand, secondHand);
 
         }
 
         if(firstHand.getHandType()[1] == 1 || firstHand.getHandType()[2] == 1 || firstHand.getHandType()[5] == 1){
 
-            Card multiplesCardFirstHand = firstHand.getMultiples().get(0).get(0);
-            Card multiplesCardSecondHand = secondHand.getMultiples().get(0).get(0);
-
-            if(getCardValueIndex(multiplesCardFirstHand.getValue()) > getCardValueIndex(multiplesCardSecondHand.getValue())){
-                return firstHand;
-            } else if(getCardValueIndex(multiplesCardFirstHand.getValue()) < getCardValueIndex(multiplesCardSecondHand.getValue())){
-                return secondHand;
-            } else {
-                return null;
-            }
+            return handleFourOfAKindAndFullHouseAndThreeOfAKind(firstHand, secondHand);
 
         }
 
         if(firstHand.getHandType()[6] == 1){
 
-            Card firstPairCardFirstHand = firstHand.getMultiples().get(0).get(0);
-            Card firstPairCardSecondHand = secondHand.getMultiples().get(0).get(0);
-            Card secondPairCardFirstHand = secondHand.getMultiples().get(1).get(0);
-            Card secondPairCardSecondHand = secondHand.getMultiples().get(1).get(0);
-
-
-            if(getCardValueIndex(firstPairCardFirstHand.getValue()) > getCardValueIndex(firstPairCardSecondHand.getValue())){
-                return firstHand;
-            } else if(getCardValueIndex(firstPairCardFirstHand.getValue()) < getCardValueIndex(firstPairCardSecondHand.getValue())){
-                return secondHand;
-            } else if(getCardValueIndex(secondPairCardFirstHand.getValue()) > getCardValueIndex(secondPairCardSecondHand.getValue())){
-                return firstHand;
-            } else if(getCardValueIndex(secondPairCardFirstHand.getValue()) < getCardValueIndex(secondPairCardSecondHand.getValue())){
-                return secondHand;
-            } else if (getCardValueIndex(firstHand.getHighestCards().get(0).getValue()) > getCardValueIndex(secondHand.getHighestCards().get(0).getValue())){
-                return firstHand;
-            } else if (getCardValueIndex(firstHand.getHighestCards().get(0).getValue()) < getCardValueIndex(secondHand.getHighestCards().get(0).getValue())){
-                return secondHand;
-            } else {
-                return null;
-            }
+            return handleTwoPair(firstHand, secondHand);
 
         }
 
         if(firstHand.getHandType()[7] == 1){
 
-            Card multiplesCardFirstHand = firstHand.getMultiples().get(0).get(0);
-            Card multiplesCardSecondHand = secondHand.getMultiples().get(0).get(0);
-            if(getCardValueIndex(multiplesCardFirstHand.getValue()) > getCardValueIndex(multiplesCardSecondHand.getValue())){
-                return firstHand;
-            } else if(getCardValueIndex(multiplesCardFirstHand.getValue()) < getCardValueIndex(multiplesCardSecondHand.getValue())){
-                return secondHand;
-            }
+            Hand firstHand1 = handlePair(firstHand, secondHand);
+            if (firstHand1 != null) return firstHand1;
         }
 
+        return handleHighestCard(firstHand, secondHand);
+
+    }
+
+    private static Hand handleHighestCard(Hand firstHand, Hand secondHand) {
         for(int i = 0; i < firstHand.getHighestCards().size(); i++){
             if(getCardValueIndex(firstHand.getHighestCards().get(i).getValue()) > getCardValueIndex(secondHand.getHighestCards().get(i).getValue())){
                 return firstHand;
@@ -107,8 +74,64 @@ public class Rules {
                 return secondHand;
             }
         }
-
         return null;
+    }
 
+    private static Hand handlePair(Hand firstHand, Hand secondHand) {
+        Card multiplesCardFirstHand = firstHand.getMultiples().get(0).get(0);
+        Card multiplesCardSecondHand = secondHand.getMultiples().get(0).get(0);
+        if(getCardValueIndex(multiplesCardFirstHand.getValue()) > getCardValueIndex(multiplesCardSecondHand.getValue())){
+            return firstHand;
+        } else if(getCardValueIndex(multiplesCardFirstHand.getValue()) < getCardValueIndex(multiplesCardSecondHand.getValue())){
+            return secondHand;
+        }
+        return null;
+    }
+
+    private static Hand handleTwoPair(Hand firstHand, Hand secondHand) {
+        Card firstPairCardFirstHand = firstHand.getMultiples().get(0).get(0);
+        Card firstPairCardSecondHand = secondHand.getMultiples().get(0).get(0);
+        Card secondPairCardFirstHand = secondHand.getMultiples().get(1).get(0);
+        Card secondPairCardSecondHand = secondHand.getMultiples().get(1).get(0);
+
+
+        if(getCardValueIndex(firstPairCardFirstHand.getValue()) > getCardValueIndex(firstPairCardSecondHand.getValue())){
+            return firstHand;
+        } else if(getCardValueIndex(firstPairCardFirstHand.getValue()) < getCardValueIndex(firstPairCardSecondHand.getValue())){
+            return secondHand;
+        } else if(getCardValueIndex(secondPairCardFirstHand.getValue()) > getCardValueIndex(secondPairCardSecondHand.getValue())){
+            return firstHand;
+        } else if(getCardValueIndex(secondPairCardFirstHand.getValue()) < getCardValueIndex(secondPairCardSecondHand.getValue())){
+            return secondHand;
+        } else if (getCardValueIndex(firstHand.getHighestCards().get(0).getValue()) > getCardValueIndex(secondHand.getHighestCards().get(0).getValue())){
+            return firstHand;
+        } else if (getCardValueIndex(firstHand.getHighestCards().get(0).getValue()) < getCardValueIndex(secondHand.getHighestCards().get(0).getValue())){
+            return secondHand;
+        } else {
+            return null;
+        }
+    }
+
+    private static Hand handleFourOfAKindAndFullHouseAndThreeOfAKind(Hand firstHand, Hand secondHand) {
+        Card multiplesCardFirstHand = firstHand.getMultiples().get(0).get(0);
+        Card multiplesCardSecondHand = secondHand.getMultiples().get(0).get(0);
+
+        if(getCardValueIndex(multiplesCardFirstHand.getValue()) > getCardValueIndex(multiplesCardSecondHand.getValue())){
+            return firstHand;
+        } else if(getCardValueIndex(multiplesCardFirstHand.getValue()) < getCardValueIndex(multiplesCardSecondHand.getValue())){
+            return secondHand;
+        } else {
+            return null;
+        }
+    }
+
+    private static Hand handleFlushAndStraight(Hand firstHand, Hand secondHand) {
+        if(getCardValueIndex(firstHand.getCards()[0].getValue()) > getCardValueIndex(secondHand.getCards()[0].getValue())){
+            return firstHand;
+        } else if(getCardValueIndex(firstHand.getCards()[0].getValue()) < getCardValueIndex(secondHand.getCards()[0].getValue())){
+            return secondHand;
+        } else {
+            return null;
+        }
     }
 }
